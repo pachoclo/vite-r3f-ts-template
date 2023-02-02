a23138104b206bf8186efe0569f2e417305d0007
Francisco Jaramillo <pacho@live.ca>
2023-02-02 11:03:29 -0500

    🏁 basic setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
+import './styles.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color='red' position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
