b4245394d93a2b0fd81dc7d5a9ce33daf0c9bb0e
Francisco Jaramillo <pacho@live.ca>
2023-02-05 20:22:14 -0500

    add pivot controls to sphere

--- src/components/Sphere.tsx
+++ src/components/Sphere.tsx
@@ -1,15 +1,34 @@
+import { PivotControls } from '@react-three/drei'
 import { useControls } from 'leva'
+import { useRef } from 'react'
+import { BufferGeometry, Group, Mesh, MeshStandardMaterial } from 'three'
 
 function Sphere() {
-  const { color } = useControls('Sphere', {
+  const sphereRef = useRef<Mesh<BufferGeometry, MeshStandardMaterial>>(null)
+  const pivotRef = useRef<Group>(null)
+
+  const { position, color, gizmo } = useControls('Sphere', {
+    position: [-2, 0, 0],
     color: 'darkorange',
+    gizmo: false,
   })
 
   return (
-    <mesh position={[-2, 0, 0]}>
-      <sphereGeometry args={[1, 30, 30]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
+    <PivotControls
+      anchor={[0, 0, 0]}
+      depthTest={false}
+      visible={gizmo}
+      ref={pivotRef}
+      onDragEnd={() => {
+        console.log(sphereRef.current?.position)
+        console.log(pivotRef.current?.position)
+      }}
+    >
+      <mesh position={position} ref={sphereRef}>
+        <sphereGeometry args={[1, 30, 30]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </PivotControls>
   )
 }
 
